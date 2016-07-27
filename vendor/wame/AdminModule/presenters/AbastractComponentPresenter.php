3d21bb3742ed897836b7afd1787e4ade39f22f0a
Linzee <d.gmiterko@gmail.com>
2016-07-27 11:09:48 +0200

    ADD abstract presenter for creating components

---
+++ vendor/wame/AdminModule/presenters/AbastractComponentPresenter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Wame\ComponentModule\Forms\ComponentForm;
+use Wame\ComponentModule\Repositories\PositionRepository;
+
+
+abstract class AbastractComponentPresenter extends ComponentPresenter
+{		
+	/** @var ComponentForm @inject */
+	public $componentForm;
+
+	/** @var PositionRepository @inject */
+	public $positionRepository;
+	
+
+	/**
+	 * Shop product component form
+	 * 
+	 * @return ComponentForm
+	 */
+	protected function createComponentEditForm()
+	{
+		$form = $this->componentForm
+						->setType($this->getComponentName())
+						->setId($this->id)
+						->build();
+		
+		return $form;
+	}
+    
+	public function renderCreate()
+	{
+		$this->template->siteTitle = _('Create '.$this->getComponentName());
+	}
+	
+	public function renderEdit()
+	{
+		$this->template->siteTitle = _('Edit '.$this->getComponentName());
+	}
+    
+    protected abstract function getComponentIdentifier();
+    
+    protected abstract function getComponentName();
+	
+}
