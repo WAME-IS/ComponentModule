9ac8ac84eb53c3fd6f5c20bd6d3b0e1e99bb5c38
Stanislav Červeňák <stano@wame.sk>
2016-07-28 09:48:26 +0200

    FIX ContainerAttributes tag is null

--- parameters/ContainerAttributes.php
+++ parameters/ContainerAttributes.php
@@ -22,6 +22,10 @@ class ContainerAttributes
             unset($attr['tag']);
         }
         
+        if ($return['tag'] == '') {
+            unset($return['tag']);
+        }
+        
         foreach ($attr as $attribute) {
             $return[$attribute['name']] = $attribute['value'];
         }
