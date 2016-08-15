153c7b932dd378a6b935043150747c22cc79136c
shadd0w <rene.gmitter@wame.sk>
2016-08-15 11:17:05 +0200

    UPDATE DeleteGridAction

--- vendor/wame/AdminModule/grids/columns/ComponentInPositionGrid/DeleteGridAction.php
+++ vendor/wame/AdminModule/grids/columns/ComponentInPositionGrid/DeleteGridAction.php
@@ -17,7 +17,7 @@ class DeleteGridAction extends BaseGridColumn
     
     
 	public function addColumn($grid) {
-		$grid->addAction('delete', '', ":Admin:Component:delete")
+		$grid->addAction('delete', '', ":Admin:Component:delete", ['id' => 'component.id'])
 			->setIcon('trash')
 			->setTitle(_('Delete'))
 			->setClass('btn btn-xs btn-danger ajax-modal');
