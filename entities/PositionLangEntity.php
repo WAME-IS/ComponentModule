3286afc0c8836721c0b759694206112df1b3fd86
Stanislav Červeňák <stano@wame.sk>
2016-09-09 12:55:35 +0200

    ADD setEntity method to PositiontLangEntity

--- entities/PositionLangEntity.php
+++ entities/PositionLangEntity.php
@@ -5,13 +5,13 @@ namespace Wame\ComponentModule\Entities;
 use Doctrine\ORM\Mapping as ORM;
 use Wame\Core\Entities\Columns;
 
+
 /**
  * @ORM\Table(name="wame_position_lang")
  * @ORM\Entity
  */
-class PositionLangEntity extends \Wame\Core\Entities\BaseEntity
+class PositionLangEntity extends \Wame\Core\Entities\BaseLangEntity
 {
-
     use Columns\Identifier;
     use Columns\Lang;
     use Columns\Title;
@@ -19,20 +19,35 @@ class PositionLangEntity extends \Wame\Core\Entities\BaseEntity
     use Columns\EditDate;
     use Columns\EditUser;
 
+    
     /**
      * @ORM\ManyToOne(targetEntity="PositionEntity", inversedBy="langs")
      * @ORM\JoinColumn(name="position_id", referencedColumnName="id", nullable=false)
      */
     protected $position;
 
-    public function setPosition($position)
-    {
-        $this->position = $position;
-        return $this;
-    }
-
-    public function getPosition($position)
+    
+	/** get ************************************************************/
+    
+    public function getPosition()
     {
         return $this->position;
     }
+    
+    
+	/** set ************************************************************/
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+        
+        return $this;
+    }
+
+    
+    public function setEntity($entity) 
+    {
+        $this->position = $entity;
+    }
+
 }
