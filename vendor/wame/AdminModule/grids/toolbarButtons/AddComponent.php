9861f731e7d9de51f8e4196c7d43f4dc90f8878b
Stanislav Červeňák <stano@wame.sk>
2016-09-23 12:27:18 +0200

    EDIT GRIDS add buttons

---
+++ vendor/wame/AdminModule/grids/toolbarButtons/AddComponent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wame\ComponentModule\Vendor\Wame\AdminModule\Grids\ToolbarButtons;
+
+use Wame\AdminModule\Vendor\Wame\DataGridControl\ToolbarButtons\Add as AdminAdd;
+
+
+class AddComponent extends AdminAdd
+{
+    public function __construct() 
+    {
+        $this->setTitle(_('Add component'));
+    }
+
+}
