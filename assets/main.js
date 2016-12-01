fac7376e8336d5478a4fd9434cdf429e62414682
Stanislav Červeňák <stano@wame.sk>
2016-12-01 07:37:37 +0100

    EDIT AddComponent

--- assets/main.js
+++ assets/main.js
@@ -10,12 +10,77 @@ $(document).ready(function(){
                 $(this).closest('li').toggle(showCurrentLi);
             });     
         });
+        
+        if ($('.component-content[data-link]') !== null) {
+            var that = $('.component-content');
+            var link = that.data('link');
 
+            if (link) {
+                that.removeData('link');
+                that.html(loader());
+                
+                $.nette.ajax({
+                    url: link,
+                    unique: false,
+                    headers: {
+                        'X-Component': true
+                    },
+                    success: function(data) {
+                        that.html(data);
+                    },
+                    error: function(error) {
+                        console.log(error);
 
+                        that.html(error.statusText);
+                    }
+                }).done(function() {
+                    WAME.init(that);
+                    initForm(that);
+                });
+            }
+        }
+    }
+});
+
+var initForm = function(object) {
+    if (object.find('form').length) {
+        object.find('form').each(function( index, form ) {
+            Nette.initForm(form);
+        });
+    }
+};
+
+var componentPushpin = function() {
+    var wrapperHeight = $('.cnt-AddComponentControl .component-info .card-panel .wrapper').outerHeight();
+    var windowHeight = $(window).outerHeight() - $('.page-content').offset().top;
+    
+    if (wrapperHeight < windowHeight) {
         $('.cnt-AddComponentControl .component-info .card-panel .wrapper').pushpin({ 
             top: $('.cnt-AddComponentControl .component-info .card-panel').offset().top,
             bottom: $('footer').offset().top - $('.cnt-AddComponentControl .component-info .card-panel .wrapper').outerHeight(true) - 20,
             offset: 70
         });
+    } else {
+
     }
-});
+};
+
+/**
+ * Loader
+ *
+ * @returns {String}
+ */
+var loader = function() {
+    var $loader = $('<div/>', {
+        'class': 'preloader valign-wrapper',
+        'html': $('<div/>', {
+            'class': 'preloader-wrapper active valign',
+            'html': $('<div/>', {
+                'class': 'spinner-layer',
+                'html': '<div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div>'
+            })
+        })
+    });
+
+    return $loader;
+};
